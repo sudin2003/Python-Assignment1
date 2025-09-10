8d69cf3e86c8dbda1563b6b189f1bb23dac3218c
sudin2003 <sudinkerkar26@gmail.com>
2025-09-10 16:08:46 +0530

    Add files via upload
    Added Task1 And Task2 Script

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b :"))
+Addition=a+b
+print("Addition :",Addition)
+Subtraction=a-b
+print("Subtraction :",Subtraction)
+Multiplication=a*b
+print("Multiplication :",Multiplication)
+Division=a/b
+print("Division :",Division)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+Name=input("Enter Your First name:")
+last_Name=input("Enter Your Last Name:")
+print("Hello,"+Name+" "+last_Name+"! welcome to the python program")
